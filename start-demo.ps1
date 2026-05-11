b92b80f8b4ff113f9c740773a35a05023fd952f5
B0cka <Bocka1423@yandex.ru>
2026-05-11 20:51:08 +0300

    удалено дублирование в notification-service

--- start-demo.ps1
+++ start-demo.ps1
@@ -1,4 +1,3 @@
-
 Write-Host "Запуск демонстрации банка..."
 
 Write-Host "готовности подов..."
@@ -8,8 +7,8 @@ Write-Host "Проброс портов для Prometheus..."
 kubectl port-forward -n bank svc/accounts-service 30090:8090 --address 0.0.0.0
 kubectl port-forward -n bank svc/api-gateway-service 30091:8700 --address 0.0.0.0
 kubectl port-forward -n bank svc/cash-service 30092:8900 --address 0.0.0.0
-kubectl port-forward -n bank svc/transfer-service 30093:9800 --address 0.0.0.0
-kubectl port-forward -n bank svc/notifications-service 30094:9900 --address 0.0.0.0
+kubectl port-forward -n bank svc/transfer-service 30094:9800 --address 0.0.0.0
+kubectl port-forward -n bank svc/notifications-service 30093:9900 --address 0.0.0.0
 
 Write-Host "Проброс Ingress для доступа к приложению..."
 kubectl port-forward -n ingress-nginx svc/ingress-nginx-controller 8080:80 --address 0.0.0.0
